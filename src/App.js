05af10a41b96c72dadcee6b4858f26f40464a465
l0o0gy <minamahone9620@gmail.com>
2024-10-16 11:48:51 +0300

    remove cover component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
-import Cover from './Components/Cover';
 
 function App() {
   return (
